04949e96f85f4c370f4e5bdd3a8193d05727bce8
dloyolar <dieegolr@gmail.com>
2023-05-11 21:00:44 -0400

    feat: 02-methods complete

---
+++ src/02-methods.ts
@@ -0,0 +1,36 @@
+export class myDate {
+  year: number
+  month: number 
+  day: number
+
+  constructor(year:number, month: number, day: number){
+    this.year = year
+    this.month = month
+    this.day = day
+  }
+
+  printFormat():string {
+    return `${this.day}/${this.month}/${this.year}`
+  }
+
+  add(amount:number, type: 'days'|'months'|'years') {
+    if(type === 'days'){
+      this.day += amount
+    }
+    if(type === 'months'){
+      this.month += amount
+    }
+    if(type === 'years'){
+      this.year += amount
+    }
+  }
+
+}
+
+const date = new myDate(2023, 3,12)
+
+console.log(date.printFormat())
+
+date.add(3, 'days')
+
+console.log(date.printFormat())
