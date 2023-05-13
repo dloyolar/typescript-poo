6b1f04de40e14fd56214c66a3509c40544c3caf8
dloyolar <dieegolr@gmail.com>
2023-05-13 13:38:01 -0400

    feat: 04-private ready

---
+++ src/04-private.ts
@@ -0,0 +1,41 @@
+export class myDate {
+  year: number;
+  month: number;
+  private day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  printFormat(): string {
+    const day = this.addPadding(this.day);
+    const month = this.addPadding(this.month);
+    return `${day}/${month}/${this.year}`;
+  }
+
+  private addPadding(value: number) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+    return `${value}`;
+  }
+
+  add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'months') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+}
+
+const date = new myDate(2023, 3, 12);
+console.log(date.printFormat());
+date.day = 10;
+date.addPadding(1);
