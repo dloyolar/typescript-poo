77f46030a6db2224a2d80780432c13eedeb535d4
dloyolar <dieegolr@gmail.com>
2023-05-15 22:05:02 -0400

    feat: 11-interfaces ready

---
+++ src/11-interfaces.ts
@@ -0,0 +1,47 @@
+export interface Driver {
+  database: string;
+  password: string;
+  port: number;
+
+  connect(): void;
+  disconnect(): void;
+  isConnected(name: string): boolean;
+}
+
+// const driver: Driver = {
+//   database: '',
+//   password: '',
+//   port: 23,
+// };
+
+class PostgresDriver implements Driver {
+  constructor(
+    public database: string,
+    public password: string,
+    public port: number
+  ) {}
+
+  connect(): void {}
+
+  disconnect(): void {}
+
+  isConnected(name: string): boolean {
+    return true;
+  }
+}
+
+class OracleDriver implements Driver {
+  constructor(
+    public database: string,
+    public password: string,
+    public port: number
+  ) {}
+
+  connect(): void {}
+
+  disconnect(): void {}
+
+  isConnected(name: string): boolean {
+    return true;
+  }
+}
