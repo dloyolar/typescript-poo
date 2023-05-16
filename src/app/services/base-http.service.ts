e1330943a95c90cc23139a18c29c973efc4b1a67
dloyolar <dieegolr@gmail.com>
2023-05-16 12:39:47 -0400

    feat: generics in methods ready

--- src/app/services/base-http.service.ts
+++ src/app/services/base-http.service.ts
@@ -1,14 +1,20 @@
 import axios from 'axios';
 import { Category } from '../models/category.model';
 import { Product } from '../models/product.model';
+import { UpdateProductDto } from '../dtos/product.dto';
 
 export class BaseHttpService<TypeClass> {
   // private data: TypeClass[] = [];
 
-  constructor(private url: string) {}
+  constructor(protected url: string) {}
 
   async getAll() {
     const { data } = await axios.get<TypeClass[]>(this.url);
+    return data;
+  }
+
+  async update<ID, DTO>(id: ID, changes: DTO) {
+    const { data } = await axios.put(`${this.url}/${id}`, { changes });
     return data;
   }
 }
@@ -25,10 +31,14 @@ export class BaseHttpService<TypeClass> {
 
   const rta = await productsService.getAll();
   console.log('products', rta.length);
+  productsService.update<Product['id'], UpdateProductDto>(1, { title: 'asd' });
 
   const url2 = 'https://api.escuelajs.co/api/v1/categories';
   const categoryService = new BaseHttpService<Category>(url2);
 
   const rta1 = await categoryService.getAll();
   console.log('categories', rta1.length);
+  categoryService.update<Category['id'], UpdateProductDto>(1, {
+    title: 'asd',
+  });
 })();
